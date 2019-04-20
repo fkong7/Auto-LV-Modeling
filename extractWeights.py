7fc6e22ffdc48f42196a5a9e5380faee4fd81e85
Fanwei Kong <fanwei_kong@ln002.brc>
2019-04-19 18:15:52 -0700

    Organized training scripts, optimized prediction script

---
+++ extractWeights.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+import tensorflow.contrib as tfcontrib
+from tensorflow.python.keras import layers
+from tensorflow.python.keras import losses
+from tensorflow.python.keras import models
+from tensorflow.python.keras import backend as K
+from loss import dice_loss, bce_dice_loss
+import os
+
+def saveWeights(model_fn,save_fn):
+    model = models.load_model(model_fn, custom_objects={'bce_dice_loss': bce_dice_loss, 'dice_loss': dice_loss})
+    model.save_weights(save_fn)
+    del model
+    K.clear_session()
+
+if __name__ == '__main__':
+    view_names = ['axial', 'coronal', 'sagittal']
+    dir_path = '/global/scratch/fanwei_kong/2DUNet/Logs'
+    base_name = ['MMWHS_small_btstrp','MMWHS_small_btstrp2','MMWHS_small_btstrp3']
+    for folder in base_name:
+        for view in view_names:
+            model_name =  'multi-all-%s_small2.hdf5' % view
+            model_path = os.path.join(dir_path, folder, model_name)
+            save_name = 'weights_' + model_name
+            save_path = os.path.join(dir_path, folder, save_name)
+            saveWeights(model_path, save_path)
+
+
