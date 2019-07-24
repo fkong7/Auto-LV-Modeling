574a9f829267bcc391bd83df29f31b08bba3900c
fkong7 <fanwei_kong@berkeley.edu>
2019-07-23 18:47:28 -0700

    Be able to export to VTK

--- label_io.py
+++ label_io.py
@@ -7,10 +7,11 @@ IO functions for importing and exporting label maps and mesh surfaces
 import SimpleITK as sitk
 import numpy as np
 import os
+import vtk
 
-def loadLabelMap(fn):
+def loadLabelMap2Py(fn):
     """ 
-    This function import the label map as numpy arrays.
+    This function imports the label map as numpy arrays.
 
     Args: 
         fn: filename of the label map
@@ -25,4 +26,182 @@ def loadLabelMap(fn):
 
     return label, spacing
 
+def loadLabelMap(fn):
+    """ 
+    This function imports the label map as sitk image.
+
+    Args: 
+        fn: filename of the label map
+
+    Return:
+        label: label map as a sitk image
+    """
+    label = sitk.ReadImage(fn)
+
+    return label
+
+def exportPy2Sitk(npArr, sitkIm):
+    """
+    This function exports the numpy array to a sitk image
+    
+    Args:
+        npArr: input numpy array
+        sitkIm: reference sitk image
+    Return:
+        outSitkIm: output sitk image
+
+    """
+    outSitkIm = sitk.GetImageFromArray(npArr)
+    outSitkIm.SetSpacing(sitkIm.GetSpacing())
+    outSitkIm.SetOrigin(sitkIm.GetOrigin())
+    outSitkIm.SetDirection(sitkIm.GetDirection())
+    return outSitkIm
+
+def writeSitkIm(sitkIm, fn):
+    """
+    This function writes a sitk image to disk
+    Args:
+        sitkIm: the sitk image to write
+        fn: file name
+    Return:
+        None
+
+    """
+    writer = sitk.ImageFileWriter()
+    writer.SetFileName(fn)
+    writer.Execute(sitkIm)
+
+    return
+
+def isoSurf2VTK(verts, faces):
+    """
+    This function creates a polydata from numpy arrays
+
+    Args:
+        verts: vertice coordinates of the isosurface
+        faces: connectivity
+    Returns:
+        poly: VTK polydata
+
+    """
+    vtkPts = vtk.vtkPoints()
+    vtkPts.SetNumberOfPoints(verts.shape[0])
+    for i in range(verts.shape[0]):
+        vtkPts.SetPoint(i, verts[i,:])
+
+    vtkCells = vtk.vtkCellArray()
+    #vtkCells.SetNumberOfCells(faces.shape[0])
+    for i in range(faces.shape[0]):
+        if faces.shape[-1]==3:
+            cell = vtk.vtkTriangle()
+        else:
+            raise ValueError('Inconsistent number of face id')
+        for j in range(faces.shape[-1]):
+            cell.GetPointIds().SetId(j, faces[i,j])
+
+        vtkCells.InsertNextCell(cell)
+    poly = vtk.vtkPolyData()
+    poly.SetPoints(vtkPts)
+    poly.SetPolys(vtkCells)
+    def _fixNormals(poly):
+        normAdj = vtk.vtkPolyDataNormals()
+        normAdj.SetInputData(poly)
+        normAdj.SplittingOff()
+        normAdj.ConsistencyOn()
+        normAdj.FlipNormalsOn()
+        normAdj.Update()
+        poly = normAdj.GetOutput()
+        return poly
+
+    poly = _fixNormals(poly)
+    return poly
+
+def writeVTKPolyData(poly, fn):
+    """
+    This function writes a vtk polydata to disk
+    Args:
+        poly: vtk polydata
+        fn: file name
+    Returns:
+        None
+    """
+    
+    print('Writing vtp with name:', fn)
+    if (fn == ''):
+        return 0
+
+    _ , extension = os.path.splitext(fn)
+
+    if extension == '.vtk':
+        writer = vtk.vtkPolyDataWriter()
+    elif extension == '.vtp':
+        writer = vtk.vtkXMLPolyDataWriter()
+    else:
+        raise ValueError("Incorrect extension"+extension)
+    writer.SetInputData(poly)
+    writer.SetFileName(fn)
+    writer.Update()
+    writer.Write()
+    return
+
+def writeVTKImage(vtkIm, fn):
+    """
+    This function writes a vtk image to disk
+    Args:
+        vtkIm: the vtk image to write
+        fn: file name
+    Returns:
+        None
+    """
+    print("Writing vti with name: ", fn)
+
+    _, extension = os.path.splitext(fn)
+    if extension == '.vti':
+        writer = vtk.vtkXMLImageDataWriter()
+    elif extension == '.mhd':
+        writer = vtk.vtkMetaImageWriter()
+    else:
+        raise ValueError("Incorrect extension " + extension)
+    writer.SetInputData(vtkIm)
+    writer.SetFileName(fn)
+    writer.Update()
+    writer.Write()
+    return
+
+def exportSitk2VTK(sitkIm):
+    """
+    This function creates a vtk image from a simple itk image
+
+    Args:
+        sitkIm: simple itk image
+    Returns:
+        vtkIm: vtk image
+    """
+    img = sitk.GetArrayFromImage(sitkIm)
+    print(img.shape)
+    importer = vtk.vtkImageImport()
+    img_data = img.astype('uint8')
+    img_string = img_data.tostring()  # type short
+    dim = img.shape
+
+    importer.CopyImportVoidPointer(img_string, len(img_string))
+    importer.SetDataScalarTypeToUnsignedChar
+    importer.SetNumberOfScalarComponents(1)
+
+    extent = importer.GetDataExtent()
+    importer.SetDataExtent(extent[0], extent[0] + dim[2] - 1,
+                                 extent[2], extent[2] + dim[1] - 1,
+                                 extent[4], extent[4] + dim[0] - 1)
+    importer.SetWholeExtent(extent[0], extent[0] + dim[2] - 1,
+                                   extent[2], extent[2] + dim[1] - 1,
+                                   extent[4], extent[4] + dim[0] - 1)
+    importer.SetDataSpacing(sitkIm.GetSpacing())
+    importer.SetDataOrigin(sitkIm.GetOrigin())
+
+    importer.Update()
+
+    vtkIm = importer.GetOutput()
+    print(vtkIm)
+    print(importer)
+    return vtkIm
 
--- main.py
+++ main.py
@@ -1,19 +1,66 @@
 import os
 import numpy as np
-from label_io import loadLabelMap
+import label_io
 from marching_cube import marching_cube
 from plot import plot_surface
+import utils
+from scipy.ndimage import gaussian_filter
 
-fn = os.path.join(os.path.dirname(__file__), "examples", "ct_train_1002_label.nii.gz")
+def test1():
+    """
+    This is a test funciton to create surface mesh from label maps with the marhing cube function from skimage
+    """
+    fn = os.path.join(os.path.dirname(__file__), "examples", "ct_train_1002_label.nii.gz")
+    
+    #load label map 
+    label = label_io.loadLabelMap(fn)
+    #convert to binary
+    pylabel = utils.convert2binary(label)
+    #debug: write to disk
+    try:
+        os.makedirs(os.path.join(os.path.dirname(__file__), "debug"))
+    except Exception as e: print(e)
+    fn_out = os.path.join(os.path.dirname(__file__), "debug", "test_volume.nii.gz")
+    label_io.writeSitkIm(label_io.exportPy2Sitk(pylabel, label), fn_out)
+    
+    #run marchine cube algorithm
+    #pylabel = gaussian_filter(pylabel, sigma=1)
+    output = marching_cube(pylabel, 0.99 )
+    verts, faces, _, _ = output
+    #write to vtk polydata
+    fn_poly = os.path.join(os.path.dirname(__file__), "debug", "test_poly.vtk")
+    label_io.writeVTKPolyData(label_io.isoSurf2VTK(verts, faces), fn_poly)
+    #plot
+    plot_surface(verts, faces, smoothed.shape)
 
-#load label map
-label, _ = loadLabelMap(fn)
-#convert to binary
-label[np.where(label!=0)] = 1
 
-#run marchine cube algorithm
-output = marching_cube(label, 0)
-verts, faces, _, _ = output
+def test2():
+    """
+    This is a test funciton to create surface mesh from label maps with the marhing cube function from skimage
+    """
+    fn = os.path.join(os.path.dirname(__file__), "examples", "ct_train_1002_label.nii.gz")
+    
+    #load label map 
+    label = label_io.loadLabelMap(fn)
+    #convert to binary
+    pylabel = utils.convert2binary(label)
+    #debug: write to disk
+    try:
+        os.makedirs(os.path.join(os.path.dirname(__file__), "debug"))
+    except Exception as e: print(e)
+    fn_out = os.path.join(os.path.dirname(__file__), "debug", "test_volume.vti")
+    label_io.writeVTKImage(label_io.exportSitk2VTK(label_io.exportPy2Sitk(pylabel, label)), fn_out)
+    
+    #run marchine cube algorithm
+    #pylabel = gaussian_filter(pylabel, sigma=1)
+    output = marching_cube(pylabel, 0.99 )
+    verts, faces, _, _ = output
+    #write to vtk polydata
+    fn_poly = os.path.join(os.path.dirname(__file__), "debug", "test_poly.vtk")
+    label_io.writeVTKPolyData(label_io.isoSurf2VTK(verts, faces), fn_poly)
+    #plot
+    plot_surface(verts, faces, smoothed.shape)
 
-#plot
-plot_surface(verts, faces)
+
+if __name__=="__main__":
+    test2()
