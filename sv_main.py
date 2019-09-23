1949197c54a128f5304c9a9de7602b2b56fc207e
fkong7 <fanwei_kong@berkeley.edu>
2019-09-23 16:50:01 -0700

    More SV meshing functions

--- sv_main.py
+++ sv_main.py
@@ -2,15 +2,15 @@ import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__),"src"))
 import argparse
-
+import meshing
+import label_io
 """
     Functions needing the SV Python interpreter
 """
-def mesh(fn, fn_out):
+def volumetric_mesh(fn, fn_out):
     """
     Example to generate volumetric mesh from surface mesh
     """
-    import meshing
     # generate volumetric mesh:
     mesh_ops = {
             'SurfaceMeshFlag': False,
@@ -23,9 +23,34 @@ def mesh(fn, fn_out):
     }
     meshing.meshPolyData(fn, fn_out, mesh_ops)
 
+def surface_and_volumetric_mesh(fn, fn_out):
+    """
+    Remesh surface mesh
+    """
+    mesh_ops = {
+            'SurfaceMeshFlag': True,
+            'VolumeMeshFlag': True,
+            'GlobalEdgeSize': 2., 
+            'MeshWallFirst': True, 
+            'NoMerge':True,
+            'NoBisect': True,
+            'Epsilon': 1e-8,
+            'Optimization': 3,
+            'QualityRatio': 1.4
+    }
+    meshing.meshPolyData(fn, fn_out, mesh_ops)
+
+
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--fn', nargs=1, help='Name of the VTK PolyData file')
-    parser.add_argument('--fn_out', nargs=1, help='Name of the volumertic file to write')
+    parser.add_argument('--fn_cap', nargs=1, help='Name of the capped VTK PolyData to write')
+    parser.add_argument('--fn_out', nargs=1, help='Name of the meshed fils to write')
     args = parser.parse_args()
-    mesh(args.fn[0], args.fn_out[0])
+    #volumetric_mesh(args.fn[0], args.fn_out[0])
+    poly = label_io.loadVTKMesh(args.fn[0])
+    ids = meshing.capPolyDataWithIds(poly, args.fn[0], args.fn_cap[0], 1, False)
+    remeshed_fn = os.path.splitext(args.fn_cap[0])[0]+'_remeshed.vtk'
+    meshing.remeshPolyData(args.fn_cap[0], remeshed_fn, 0.9, 1.1, True)
+    surface_and_volumetric_mesh(remeshed_fn, args.fn_out[0]) 
+
