27b2b58e9a42e676ac98f975aa12caaa9272cc30
fkong7 <fanwei_kong@berkeley.edu>
2019-09-24 19:36:00 -0700

    class wrapped registration

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def buildSurfaceModelFromImage(fns, fns_out):
     """
     FACTOR_LA = 0.7
     FACTOR_AA = 1.2
-    MESH_RESOLUTION = (2.,2.,2.)
+    MESH_RESOLUTION = (1.5,1.5,1.5)
 
     for fn in fns: 
 
@@ -47,57 +47,11 @@ def buildSurfaceModelFromImage(fns, fns_out):
         model.processCap(1.5) 
         fn = os.path.join(os.path.dirname(__file__), "debug", "temp.vtk")
         model.writeSurfaceMesh(fn)
-        model.remesh(2., fn, fns_out)
+        model.remesh(1.5, fn, fns_out)
         model.writeSurfaceMesh(fns_out[0])
-        model.writeVolumeMesh(fns_out[1])
+        #model.writeVolumeMesh(fns_out[1])
         return model
 
-
-def registration(START_PHASE, TOTAL_PHASE, MODEL_NAME, IMAGE_NAME, output_dir, seg_fn, fn):
-    """
-    Registration of surface mesh point set using Elastix
-    Performs 3D image registration and move points based on the computed transform
-    Cap the surface mesh with test6_2()
-    """
-    import registration
-    import SimpleITK as sitk
-    
-    # compute volume of all phases to select systole and diastole:
-    volume = list()
-    # build surface mesh from segmentation at START_PHASE
-    fn_poly = os.path.join(output_dir, MODEL_NAME % START_PHASE)
-    model, cap_pts_ids = buildSurfaceModelFromImage([seg_fn], fn_poly)
-    volume.append(utils.getPolydataVolume(model))
-    
-    ids = list(range(START_PHASE,TOTAL_PHASE)) + list(range(0,START_PHASE))
-    # Only need to register N-1 mesh
-    for index in ids[:-1]:
-        print("REGISTERING FROM %d TO %d " % (START_PHASE, (index+1)%TOTAL_PHASE))
-    
-        #ASSUMING increment is 1
-        moving_im_fn = os.path.join(image_dir, IMAGE_NAME % ((index+1)%TOTAL_PHASE)) 
-        fixed_im_fn =os.path.join(image_dir, IMAGE_NAME % START_PHASE)
-        
-        fn_out = os.path.join(output_dir, "verts.pts")
-
-        new_model = registration.point_image_transform(utils.resample(sitk.ReadImage(fixed_im_fn)),
-            utils.resample(sitk.ReadImage(moving_im_fn)),
-            model,
-            fn_out
-        )
-        # Project the cap points so that they are co-planar
-        for pt_ids in cap_pts_ids:
-            pts = utils.getPolyDataPointCoordinatesFromIDs(new_model, pt_ids)
-            new_model = utils.projectOpeningToFitPlane(new_model, pt_ids, pts, 3)
-        #ASSUMING increment is 1
-        fn_poly = os.path.join(output_dir, MODEL_NAME % ((index+1)%TOTAL_PHASE))
-        label_io.writeVTKPolyData(new_model, fn_poly)
-        volume.append(utils.getPolydataVolume(new_model))
-
-    SYSTOLE_PHASE = ids[np.argmin(volume)]
-    DIASTOLE_PHASE = ids[np.argmax(volume)]
-    print("systole, diastole: ", SYSTOLE_PHASE, DIASTOLE_PHASE)
-    return DIASTOLE_PHASE
 
     
 def test1():
--- src/registration.py
+++ src/registration.py
@@ -3,42 +3,78 @@ import numpy as np
 import SimpleITK as sitk
 import vtk
 import label_io
-
-def point_image_transform(fixed_im, moving_im, poly, fn):
+import models
+class Registration:
     """
-    Register one 3D volume to another, 
-    Transform the points using the computed transformation
-
-    Args:
-        fixed_im: target image (sitk Image)
-        moving_im: moving image (sikt Image)
-        poly: surface mesh to transform (vtk PolyData)
-        fn: file name to write the vertices of polydata to file
-
-    Returns:
-        new_poly: transformed surface mesh (vtk PolyData)
+    Class to perform 3D image registration
     """
-    label_io.writeVTKPolyDataVerts(poly, fn)
-    # Compute the transformation
-    elastixImageFilter = sitk.ElastixImageFilter()
-    elastixImageFilter.SetFixedImage(fixed_im)
-    elastixImageFilter.SetMovingImage(moving_im)
-    elastixImageFilter.Execute()
+    def __init__(self, fixed_im_fn=None, moving_im_fn=None):
+        """
 
-    # wrap point set
+        Args:
+            fixed_im_fn: target image fn
+            moving_im_fn: moving image fn
+        """
+        self.fixed_fn = fixed_im_fn
+        self.moving_fn = fixed_im_fn
+        self.fixed = None
+        self.moving = None
+        self.registration_filter = None
 
-    transformixImageFilter = sitk.TransformixImageFilter()
-    transformixImageFilter.SetMovingImage(moving_im)
-    transformixImageFilter.SetTransformParameterMap(elastixImageFilter.GetTransformParameterMap())
-    transformixImageFilter.SetFixedPointSetFileName(fn)
-    transformixImageFilter.SetOutputDirectory(os.path.dirname(fn))
-    transformixImageFilter.Execute()
+    def updateMovingImage(self, moving_im_fn):
+        self.moving_fn = moving_im_fn
+        self.moving = None
 
-    # build VTK PolyData
-    pts = label_io.readElastixPointOuptut(os.path.join(os.path.dirname(fn),'outputpoints.txt'))
+    def updateFixedImage(self, fixed_im_fn):
+        self.fixed_fn = fixed_im_fn
+        self.fixed = None
 
-    new_poly = vtk.vtkPolyData()
-    new_poly.DeepCopy(poly)
-    new_poly.SetPoints(pts)
-    return new_poly
+    def loadImages(self):
+        self.fixed = sitk.ReadImage(self.fixed_fn)
+        self.moving = sitk.ReadImage(self.moving_fn)
+
+    def computeTransform(self):
+
+        if (self.fixed is None) or (self.moving is None):
+            self.loadImages()
+        elastixImageFilter = sitk.ElastixImageFilter()
+        elastixImageFilter.SetFixedImage(fixed_im)
+        elastixImageFilter.SetMovingImage(moving_im)
+        elastixImageFilter.Execute()
+
+        self.registration_filter = elastixImageFilter
+
+    def polydata_image_transform(self, model, fn):
+        """
+        Transform the points of a geometry using the computed transformation
+        
+        Args:
+            poly: surface mesh to transform (vtk PolyData)
+            fn: file name to write the vertices of polydata to file
+
+        Returns:
+            new_poly: transformed surface mesh (vtk PolyData)
+        """
+
+        label_io.writeVTKPolyDataVerts(model.poly, fn)
+        if self.registration_filter is None:
+            self.computeTransform()
+        # wrap point set
+        transformixImageFilter = sitk.TransformixImageFilter()
+        transformixImageFilter.SetMovingImage(moving_im)
+        transformixImageFilter.SetTransformParameterMap(self.registration_filter.GetTransformParameterMap())
+        transformixImageFilter.SetFixedPointSetFileName(fn)
+        transformixImageFilter.SetOutputDirectory(os.path.dirname(fn))
+        transformixImageFilter.Execute()
+
+        # build VTK PolyData
+        pts = label_io.readElastixPointOuptut(os.path.join(os.path.dirname(fn),'outputpoints.txt'))
+
+        new_poly = vtk.vtkPolyData()
+        new_poly.DeepCopy(poly)
+        new_poly.SetPoints(pts)
+        return models.leftVentricle(model.update(new_poly))
+
+
+        
 
