2e7559b81412eb9e2239bd7799d13cf00d4d1f88
fkong7 <fanwei_kong@berkeley.edu>
2019-09-26 12:06:28 -0700

    Bug fix and volume mesh script

--- bash_scripts/main.sh
+++ bash_scripts/main.sh
@@ -4,11 +4,14 @@ json_file=/Users/fanweikong/Documents/Modeling/SurfaceModeling/info.json
 
 model_script=/Users/fanweikong/Documents/Modeling/SurfaceModeling/main.py
 registration_script=/Users/fanweikong/Documents/Modeling/SurfaceModeling/elastix_main.py
+volume_mesh_script=/Users/fanweikong/Documents/Modeling/SurfaceModeling/volume_mesh_main.py
 
 #${sv_python_dir}/sv --python -- ${model_script} --json_fn ${json_file}
 
 
-conda activate elastix
-python ${registration_script} --json_fn ${json_file} --write
-conda deactivate
+#conda activate elastix
+#python ${registration_script} --json_fn ${json_file} --write
+#conda deactivate
+
+${sv_python_dir}/sv --python -- ${volume_mesh_script} --json_fn ${json_file}
 
