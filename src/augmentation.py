bb750dafe1b165d6b2e91747bf466758995b4981
Fanwei Kong <fanwei_kong@berkeley.edu>
2019-10-17 23:26:10 -0700

    Added more augmentations

--- src/augmentation.py
+++ src/augmentation.py
@@ -29,24 +29,42 @@ def flip_img(horizontal_flip, tr_img, label_img):
                                 lambda: (tf.image.flip_left_right(tr_img), tf.image.flip_left_right(label_img)),
                                 lambda: (tr_img, label_img))
   return tr_img, label_img
+"""## Rotate the image with random angle""" 
+def rotate_img(rotation, tr_img, label_img):
+    if rotation:
+        angle = rotation * 3.1415926 / 180.
+        angle_rand = tf.random_uniform([], -1*angle, angle)
+        tf_img = tf.contrib.image.rotate(tr_img, angle_rand)
+        label_img = tf.contrib.image.rotate(label_img, angle_rand)
+    return tr_img, label_img
 
 """##Scale/shift the image intensity randomly"""
 
-def changeIntensity_img(tr_img, label_img, changeIntensity=False):
-  if changeIntensity:
-    scale = tf.random_uniform([], 0.9, 1.1)
-    shift = tf.random_uniform([], -0.1, 0.1)
+def changeIntensity_img(tr_img, label_img, change):
+  if change:
+    scale = tf.random_uniform([], change['scale'][0], change['scale'][1])
+    shift = tf.random_uniform([], change['shift'][0], change['shift'][1])
     tr_img = tr_img*scale+shift
-    
+    tr_img = tf.clip_by_value(tr_img, -1., 1.)
   return tr_img, label_img
 
 """##Scale the image intensity for different cardiac structures"""
+def change_class_intensity_img(tr_img, label_img, change, num_class):
+    if change:
+        for i in range(1, num_class):
+            scale = tf.random_uniform([], change['scale'][0], change['scale'][1])
+            shift = tf.random_uniform([], change['shift'][0], change['shift'][1])
+            scaled = tr_img*scale+shift
+            tr_img = tf.where(tf.equal(label_img,i), scaled, tr_img)
+    return tr_img, label_img
 """## Assembling our transformations into our augment function"""
 
 def _augment(img,
              label_img,
+             num_class =0, 
              resize=None,  # Resize the image to some size e.g. [256, 256]
              horizontal_flip=False,  # Random left right flip,
+             rotation = 0, 
              changeIntensity=False,
              width_shift_range=0,  # Randomly translate the image horizontally
              height_shift_range=0):  # Randomly translate the image vertically 
@@ -55,7 +73,9 @@ def _augment(img,
     label_img = tf.image.resize_images(label_img, resize, method=tf.image.ResizeMethod.NEAREST_NEIGHBOR, align_corners=True)
     img = tf.image.resize_images(img, resize, method=tf.image.ResizeMethod.NEAREST_NEIGHBOR, align_corners=True)
   img, label_img = flip_img(horizontal_flip, img, label_img)
+  img, label_img = rotate_img(rotation, img, label_img)
   img, label_img = shift_img(img, label_img, width_shift_range, height_shift_range)
   img, label_img = changeIntensity_img(img, label_img,changeIntensity )
+  img, label_img = change_class_intensity_img(img, label_img, changeIntensity, num_class) 
   return img, label_img
 
