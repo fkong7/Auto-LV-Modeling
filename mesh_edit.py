76c3e388bd7285dac492d872f80caea220459cf5
Fanwei <fanwei_kong@berkeley.edu>
2020-03-23 23:30:01 -0700

    Build LV template

--- mesh_edit.py
+++ mesh_edit.py
@@ -51,17 +51,176 @@ def insertPtByNodeId(source, target, pt_id):
     return target
 
 def build_template():
-    poly_fn = '/Users/fanweikong/Documents/Modeling/pycpd/data/meshes/multi_dataset/ct_train_1010_label.nii.gz.vtk'
-    poly_tmplt_fn = '/Users/fanweikong/Documents/Modeling/pycpd/data/meshes/ct_train_10_lvcfd_tmplt.vtp'
+    poly_fn = '/Users/fanweikong/SurfaceModeling/template/ct_train_1010_label.nii.gz.vtk'
+    poly_tmplt_fn = '/Users/fanweikong/SurfaceModeling/template/ct_train_10_lvcfd_tmplt.vtp'
     poly = label_io.loadVTKMesh(poly_fn)
     poly_tmplt =label_io.loadVTKMesh(poly_tmplt_fn)
     label_io.writeVTKPolyData(poly_tmplt, poly_tmplt_fn)
 
-    print(poly_tmplt.GetNumberOfPoints())
-    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 1606)
-    print("ok")
-    print(poly_tmplt.GetNumberOfPoints())
+    #NODE Betwen AV and MV
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 3324)
+    new_id = poly_tmplt.GetNumberOfPoints() - 1
+
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 317)
+    first_id = poly_tmplt.GetNumberOfPoints() - 1
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 30, 36)
+    
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 314)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 36)
     poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 36, 2)
+    
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 3708)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 2)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 2, 43)
+    
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 4510)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 43)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 43, 39)
+    
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 4190)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 39)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 39, 6)
+    
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 4343)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 6)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 6, 8)
+    
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 4222)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 8)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 8, new_id) 
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 3046)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, new_id)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, new_id, 30)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, first_id, poly_tmplt.GetNumberOfPoints()-1, 30)
+
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 1008)
+    first_id = poly_tmplt.GetNumberOfPoints()-1
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 48, 49)
+    
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 4234)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 49)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 49, 50)
+    
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 3475)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 50)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 50, 51)
+    
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 2329)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 51)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 51, 52)
+
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 4540)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 52)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 52, 53)
+
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 3628)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 53)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 53, 54)
+
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 2967)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 54)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 54, 47)
+
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 1378)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 47)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 47, 48)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, first_id, poly_tmplt.GetNumberOfPoints()-1, 48)
+    
+    
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 279)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 42, 27)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 27, 30)
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 2688)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 30)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 30, new_id)
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 3720)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 30, new_id)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, new_id)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, new_id, 8)
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 3300)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 8)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 8, 20)
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 2923)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 20, 42)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 20)
+    
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 3767)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 67, 42)
+    
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 2754)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 42)
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 591)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 42)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 42, 63)
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 615)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 63)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 63, 64)
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 468)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 64)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 64, 65)
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 3488)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 65)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 65, 66)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 66, 67)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 67, 68)
+    
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 3467)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 68, 69)
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 1876)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 69)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 69, 70)
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 883)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 70)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 70, 71)
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 166)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 71)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 71, 72)
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 1878)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 72)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 72, 73)
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 3583)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 73)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 73, 68)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 68, 74)
+    
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 2735)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 74, 75)
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 1342)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 75, 76)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 75)
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 740)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 76, 77)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 76)
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 881)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 77, 78)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 77)
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 1942)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 78, 79)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 78)
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 3275)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 79, 74)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 79)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 74, 80)
+
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 4399)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 85, 80)
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 1669)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 80, 81)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 80)
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 780)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 81, 82)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 81)
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 655)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 82, 83)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 82)
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 704)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 83, 84)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 83)
+    poly_tmplt = insertPtByNodeId(poly, poly_tmplt, 4593)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 84, 85)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, poly_tmplt.GetNumberOfPoints()-2, 84)
+    poly_tmplt = insertTriByNodeIds(poly_tmplt, poly_tmplt.GetNumberOfPoints()-1, 85, 86)
+
     
     label_io.writeVTKPolyData(poly_tmplt, os.path.join(os.path.dirname(poly_tmplt_fn), 'tmplt.vtp'))
     label_io.writeVTKPoints(poly_tmplt.GetPoints(), os.path.join(os.path.dirname(poly_tmplt_fn), 'tmplt_pts.vtp'))
