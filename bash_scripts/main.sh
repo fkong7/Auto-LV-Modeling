d9b30415ab28c01a4d590dffce880aab5db15dfb
Fanwei <fanwei_kong@berkeley.edu>
2020-08-18 12:42:31 -0700

    Origanize

--- bash_scripts/main.sh
+++ bash_scripts/main.sh
@@ -7,21 +7,13 @@ model_script=/Users/fanweikong/Documents/Modeling/SurfaceModeling/main.py
 registration_script=/Users/fanweikong/Documents/Modeling/SurfaceModeling/elastix_main.py
 volume_mesh_script=/Users/fanweikong/Documents/Modeling/SurfaceModeling/volume_mesh_main.py
 
-#${sv_python_dir}/sv --python -- ${model_script} --json_fn ${json_file}
-
-#dir=/Users/fanweikong/Downloads/test_ensemble-2-10-2
-#dir=/Users/fanweikong/Documents/Modeling/SurfaceModeling/examples
-#dir=/Users/fanweikong/Documents/ImageData/orCalScore_CTAI/ct_train_masks
-#dir=/Users/fanweikong/Documents/ImageData/4DCCTA/MACS40244_20150309/wall_motion_labels_gt
-#dir=/Users/fanweikong/Documents/ImageData/MMWHS/test_ensemble-2-10-2_seg_corrected
-#dir=/Users/fanweikong/Documents/ImageData/MMWHS/temp
-dir=/Users/fanweikong/Documents/Modeling/SurfaceModeling/results/test_ensemble_4_20_seg
+dir=./examples
 ls ${dir}
 for file in ${dir}/*.nii.gz; do echo ${file} &&  ${sv_python_dir}/simvascular --python -- ${model_script} --json_fn ${json_file} --seg_name ${file##*/}; done
 
-#conda activate elastix
-#python ${registration_script} --json_fn ${json_file} --write --smooth
-#conda deactivate
+conda activate elastix
+python ${registration_script} --json_fn ${json_file} --write --smooth
+conda deactivate
 
-#${sv_python_dir}/sv --python -- ${volume_mesh_script} --json_fn ${json_file}
+${sv_python_dir}/sv --python -- ${volume_mesh_script} --json_fn ${json_file}
 
