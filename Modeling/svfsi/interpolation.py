b1e9f52a82526e7982226beab15211fa8163f126
Fanwei <fanwei_kong@berkeley.edu>
2025-05-29 13:26:45 -0500

    option to complete the cardiac cycle

--- Modeling/svfsi/interpolation.py
+++ Modeling/svfsi/interpolation.py
@@ -10,7 +10,7 @@ import io_utils
 import utils
 import re
 import vtk
-from scipy.interpolate import CubicSpline
+from scipy.interpolate import CubicSpline, UnivariateSpline, Akima1DInterpolator
 
 """
 Functions to write interpolated surface meshes for perscribed wall motion
@@ -96,36 +96,81 @@ def move_mesh_cubic_hermite(meshes, start_point, intpl_num, num_cycle):
     
     return store
 
-def move_mesh_cubic(meshes, start_point, intpl_num, num_cycle):
+def move_mesh_cubic(meshes, start_point, intpl_num, num_cycle, start_percent, rr_percent):
     total_num_phase = len(meshes)
     total_steps = total_num_phase * (intpl_num+1)*num_cycle
+    steps_per_cycle = total_num_phase * (intpl_num+1) 
     poly_template = meshes[start_point]
     ref_coords = vtk_to_numpy(poly_template.GetPoints().GetData())
     store = np.zeros((poly_template.GetNumberOfPoints(), 3, total_steps+1)) 
     count = 0
+    time_list = np.linspace(start_percent, start_percent+rr_percent, total_num_phase, endpoint=False)
+    print("Time list initial: ", time_list)
+    if rr_percent < 0.9:
+        mid_time = ((1.-rr_percent)/2.+(start_percent+rr_percent))% 1.
+        mid_mesh = vtk.vtkPolyData()
+        mid_mesh.DeepCopy(meshes[0])
+        mid_mesh.GetPoints().SetData(numpy_to_vtk(\
+                0.5*vtk_to_numpy(meshes[0].GetPoints().GetData()) + \
+                0.5*vtk_to_numpy(meshes[-1].GetPoints().GetData())))
+        meshes.append(mid_mesh)
+        time_list = np.append(time_list, mid_time)
+        total_num_phase += 1
+    print("Time list after insert mid point: ", time_list)
     mesh_list = [meshes[idx] for idx in list(range(start_point, total_num_phase)) +\
             list(range(0, start_point))]
     mesh_list = [mesh_list[-3], mesh_list[-2], mesh_list[-1]] + mesh_list + \
             [mesh_list[0], mesh_list[1], mesh_list[2]]
+    time_list = [time_list[idx] for idx in list(range(start_point, total_num_phase)) +\
+            list(range(0, start_point))]
+    time_list = [time_list[-3], time_list[-2], time_list[-1]] + time_list + \
+            [time_list[0], time_list[1], time_list[2]]
+
+    # make sure the time increase monotonically.
+    print("Time list after reordering: ", time_list)
+    for i, t in enumerate(time_list[1:]):
+        diff = t - time_list[i]
+        if diff < 0:
+            time_list[i+1] += np.ceil(-diff)
+    print("Time list after monotonic increase: ", time_list)
+
     mesh_point_list = [vtk_to_numpy(m.GetPoints().GetData()) for m in mesh_list]
     mesh_points = np.array(mesh_point_list)
-    time_list = [-3, -2, -1] + list(range(total_num_phase)) + \
-            [total_num_phase, total_num_phase+1, total_num_phase+2]
     
+    # Prepare storage for interpolated points
+    num_points = mesh_points.shape[1]
+    dim = mesh_points.shape[-1]
+    time_interp = np.linspace(time_list[3], time_list[-3], steps_per_cycle, endpoint=False)
+    new_coords = np.zeros((len(time_interp), num_points, dim))
+    print("TIME INTERP: ", time_interp)
+    # Apply Akima interpolation (C1) for each mesh point and each dimension
+    #for point_idx in range(num_points):
+    #    for d in range(dim):  # Iterate over x, y, z
+    #        akima = Akima1DInterpolator(time_list, mesh_points[:, point_idx, d])
+    #        new_coords[:, point_idx, d] = akima(time_interp)
+    #new_coords = new_coords.transpose(1, 2, 0) 
+
+    # Cubic Spline (C2)
     cs = CubicSpline(np.array(time_list), mesh_points)
-    
-    time_interp = np.linspace(0, total_num_phase, total_num_phase * (intpl_num+1))
     new_coords = cs(time_interp).transpose(1, 2, 0)
     displacements = new_coords - np.expand_dims(ref_coords, axis=-1)
+    #plt.plot(time_interp, new_coords[1000, 0, :])
+    #print(mesh_points.shape)
+    #plt.plot(np.array(time_list), mesh_points.transpose(1,2,0)[1000, 0, :], marker='o')
+    #plt.show()
+    print(displacements.shape)
     
     ## The rest cycles are copies of first cycle
+    if rr_percent < 0.9:
+        total_num_phase = total_num_phase-1 
     for c in range(num_cycle):
-        s = c*total_num_phase * (intpl_num+1)
+        s = c * total_num_phase * (intpl_num+1)
         e = s + total_num_phase * (intpl_num+1)
+        print(s, e)
         store[:,:,s:e] = displacements
     return store
 
-def write_motion(meshes,  start_point, intpl_num, output_dir, num_cycle, duration, debug=False, scale=1.):
+def write_motion(meshes,  start_point, intpl_num, output_dir, num_cycle, duration, start_percent, rr_percent, debug=False, scale=1.):
     total_num_phase = len(meshes)
     total_steps = num_cycle* total_num_phase * (intpl_num+1)+1
     initialized = False
@@ -133,8 +178,7 @@ def write_motion(meshes,  start_point, intpl_num, output_dir, num_cycle, duratio
     
     poly_template = meshes[start_point]
     
-    displacements = move_mesh_cubic(meshes, start_point, intpl_num, num_cycle)
-    #displacements = move_mesh_cubic_hermite(meshes, start_point, intpl_num, num_cycle)
+    displacements = move_mesh_cubic(meshes, start_point, intpl_num, num_cycle, start_percent, rr_percent)
     if debug:
         debug_dir = os.path.join(output_dir,"Debug")
         try:
@@ -144,7 +188,7 @@ def write_motion(meshes,  start_point, intpl_num, output_dir, num_cycle, duratio
         poly = vtk.vtkPolyData()
         poly.DeepCopy(poly_template)
         for ii in range(displacements.shape[-1]):
-            poly.GetPoints().SetData(numpy_to_vtk(displacements[:,:,ii] * scale + coords * scale))
+            poly.GetPoints().SetData(numpy_to_vtk(displacements[:,:,ii] + coords))
             fn_debug = os.path.join(debug_dir, "debug%05d.vtp" %ii)
             io_utils.write_vtk_polydata(poly, fn_debug)
 
@@ -196,6 +240,8 @@ if __name__=='__main__':
             file that can be used to make sure the correspondence between the simulation mesh input and \
             the motion meshes are correct")
     parser.add_argument('--phase', default=-1, type=int, help="Id of the phase to generate volume mesh")
+    parser.add_argument('--start_percent', type=float, help="Percentage of Cardiac cycle when imaging starts")
+    parser.add_argument('--rr_percent', type=float, help="Percentage of cardiac cycle during which the images were acquired")
     args = parser.parse_args()
     
     mesh_dir = args.input_dir
@@ -210,8 +256,8 @@ if __name__=='__main__':
     meshes = [io_utils.read_vtk_mesh(f) for f in fns]
 
     if args.mesh_complete_surface is not None:
-        print("!!!!!!!!", args.mesh_complete_surface)
-        print("!!!!!!!!", fns[args.phase])
+        print("!!!!!!!! mesh complete", args.mesh_complete_surface)
+        print("!!!!!!!! surf", fns[args.phase])
         surf_ori = io_utils.read_vtk_mesh(args.mesh_complete_surface)
         id_list = utils.find_point_correspondence(surf_ori, meshes[args.phase].GetPoints())
         # check if correspondence can be established
@@ -225,6 +271,7 @@ if __name__=='__main__':
         else:
             meshes[args.phase].GetPoints().SetData(surf_ori.GetPoints().GetData())
         for ind, m in enumerate(meshes):
+            print("DEBUG: ", m.GetNumberOfPoints(), surf_ori.GetNumberOfPoints())
             points = vtk_to_numpy(m.GetPoints().GetData())
             points_new = points[id_list, :]
             m = vtk.vtkPolyData()
@@ -232,6 +279,6 @@ if __name__=='__main__':
             m.GetPoints().SetData(numpy_to_vtk(points_new))
             meshes[ind] = m
 
-    write_motion(meshes,  args.phase ,args.num_interpolation, output_dir, args.num_cycle, args.duration, debug=False, scale=args.scale)
+    write_motion(meshes,  args.phase ,args.num_interpolation, output_dir, args.num_cycle, args.duration, args.start_percent, args.rr_percent, debug=False, scale=args.scale)
     end = time.time()
     print("Time spent: ", end-start)
